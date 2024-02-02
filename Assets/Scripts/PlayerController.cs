2bf3cc3def43e9b7425a911fbdc8695d936e0667
imcookiedabadee <ohitscookiemfl@gmail.com>
2024-02-02 16:31:29 -0500

    changed movement to keyboard only
    no more mouse camera movement, player turns faces move direction (cube helps to show that)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,30 +2,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 0f; 
-    public float lookSpeed = 0f; 
-    private CharacterController controller;
-    private float pitch = 0.0f;
+    [SerializeField] private float moveSpeed = 30f; 
+    [SerializeField] private float rotateSpeedMultiplier = 10f; 
+    private Rigidbody rb;
 
+    Vector3 input;
     // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal") * moveSpeed;
-        float moveVertical = Input.GetAxis("Vertical") * moveSpeed;
-        Vector3 move = transform.right * moveHorizontal + transform.forward * moveVertical;
-        controller.Move(move * Time.deltaTime);
+        input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
 
-        transform.Rotate(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        if (input.magnitude > 0)
+        {
+            float angle = Vector3.SignedAngle(transform.forward, input.normalized, transform.up);
+            rb.transform.Rotate(Vector3.up * angle * rotateSpeedMultiplier * Time.deltaTime);
+        }
+        rb.velocity = input.normalized * moveSpeed * Time.deltaTime;
 
-        pitch -= Input.GetAxis("Mouse Y") * lookSpeed;
-        pitch = Mathf.Clamp(pitch, -45.0f, 45.0f);
-        Camera.main.transform.localEulerAngles = new Vector3(pitch, transform.eulerAngles.y, 0);
     }
 }
 
