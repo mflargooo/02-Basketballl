b04697c2c3d2e1a8c73846c3d49dedad48f35df6
Apple <81790850+Sunzhuofan-Apple@users.noreply.github.com>
2024-02-03 17:48:15 -0500

    slider

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -1,19 +1,20 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections; 
 
 public class ShootingWithReset : MonoBehaviour
 {
-    public Slider bar; 
-    public RectTransform targetIndicator; 
-    public GameObject successUI; 
-    public float moveSpeed = 0.5f; 
-    private float targetValue = 0.5f; 
-    private bool moving = false;
+    public Slider bar;
+    public RectTransform targetIndicator;
+    public GameObject successUI;
+    private float acceleration = 0f; 
+    private float targetValue = 0.5f;
+    private bool isPressing = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        ResetGame(); 
+        ResetGame();
     }
 
     // Update is called once per frame
@@ -21,29 +22,44 @@ public class ShootingWithReset : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            moving = !moving; 
+            isPressing = true;
+            acceleration = 0f; // reset acc
         }
-        if (moving)
-        {
-            bar.value = Mathf.Lerp(bar.value, targetValue, Time.deltaTime * moveSpeed);
 
-            if (Mathf.Abs(bar.value - targetValue) < 0.05f)//0.05
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isPressing = false;
+        }
+
+        if (isPressing)
+        {
+            acceleration += Time.deltaTime; 
+            bar.value += acceleration * Time.deltaTime; 
+        }
+        else if (acceleration > 0)
+        {
+      
+            bar.value += acceleration * Time.deltaTime;
+            acceleration *= 0.9f; 
+
+            if (acceleration < 0.01f)
             {
-                ShowSuccessUI(); 
-                ResetGame(); 
+                acceleration = 0f; 
+                CheckSuccess(); 
             }
         }
     }
+
     void ResetGame()
     {
-        moving = false; 
-        bar.value = 0; 
-        SetTargetValue(); 
-        successUI.SetActive(false); 
+        bar.value = 0;
+        SetTargetValue();
+        successUI.SetActive(false);
     }
+
     void SetTargetValue()
     {
-        targetValue = Random.Range(0f, 1f); 
+        targetValue = Random.Range(0.25f, 1f); 
         UpdateTargetIndicatorPosition();
     }
 
@@ -56,9 +72,25 @@ public class ShootingWithReset : MonoBehaviour
         }
     }
 
-    void ShowSuccessUI()
+    void CheckSuccess()
     {
-        successUI.SetActive(true); 
+
+        if (Mathf.Abs(bar.value - targetValue) < 0.05f) 
+        {
+            StartCoroutine(ShowSuccessAndReset());
+        }
+        else
+        {
+            ResetGame();
+        }
+    }
+
+    IEnumerator ShowSuccessAndReset()
+    {
+        successUI.SetActive(true);
+        yield return new WaitForSeconds(1);
+        ResetGame();
     }
 }
+
 
